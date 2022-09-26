a952a6dff4ff84d00894d447e97b8e1e8f1b1cfa
ks22ss <107049203+ks22ss@users.noreply.github.com>
2022-09-26 17:24:42 +0800

    Update oop_backtest.py

--- oop_backtest.py
+++ oop_backtest.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import itertools
+from typing import Dict, List, Tuple
+from abc import ABC, abstractmethod
+import numpy as np
+
+def weighted_average(list_of_tuples: List[Tuple[float, float]]):
+    """[(weight, measurement), ...]"""
+    if len(list_of_tuples) == 0:
+        return 0
+    
+    sum_of_weight = sum([w for w,m in list_of_tuples])
+    
+    if sum_of_weight == 0:
+        return 0
+        
+    return sum([(w/sum_of_weight)*m for w,m in list_of_tuples])
+    
+print(weighted_average([(50,10),(20,11),(-70, 12)]))
 
 
-class BacktestRunner():
+class BacktestRunner(ABC):
     
     def __init__(self, df, warm_up, lot_size, debug=True):
         self.debug = debug
@@ -10,17 +27,21 @@ class BacktestRunner():
         self.warm_up = warm_up
         self.lot_size = lot_size
         
-        self.pos_states = {
-            'pos_lots': 0,
-            'pos_open_price': 0,
-            'pos_close_price': 0
-        }
+        self.pos_list = [(0,0)]
+
         self.records = {
+            'trade_open_list': [],
+            'trade_close_list': [],
             'pnl_list' : [],
             'equity_list' : [],
             'dd_dollar_list' : [],
             'dd_pct_list' : []
         }
+    
+    @abstractmethod    
+    def onData(self, i, params):
+        return
+        
         
     def market_data(self, i, name):
         if name in self.df.columns:
@@ -28,55 +49,46 @@ class BacktestRunner():
         else:
             raise Exception(f"Column name {name} not found in df.")
     
-    def get_pos_state(self, name):
-        """
-        pos_lots(Int): Number of Lots in current Position
-        pos_open_price(Float): Open Price of current Position
-        pos_close_price(Float): Close Price of current Position
-        """
-        return self.pos_states[name]
-    
     def open_position(self, pos_open_price, pos_open_lots):
-        print("Opening Position...")
+
+
+        # updated total position lots and average open price after opening position
+        self.pos_list.append((pos_open_lots, pos_open_price))
+        avg_open_price = weighted_average(self.pos_list)
+        pos_lots = self.pos_list[0][0] + pos_open_lots
+        self.pos_list = []
+        self.pos_list.append((pos_lots, avg_open_price))
         
-        self.pos_states['pos_lots'] = pos_open_lots
-        self.pos_states['pos_open_price'] = pos_open_price
         
         if self.debug:
             print(f"""\n
-                Position Open @ Price = {self.pos_states['pos_open_price']}
+                [Position Open] {pos_open_lots} Lots @ Price = {pos_open_price}
             """)
+        self.records['trade_open_list'].append((pos_open_lots, pos_open_price))
     
     
     def close_position(self, pos_close_price, pos_close_lots):
-        print("Closing Position...")
-        
-        self.pos_states['pos_close_price'] = pos_close_price
-        
-        pnl_share = (self.pos_states['pos_close_price'] - self.pos_states['pos_open_price']) 
-        shares = pos_close_lots * self.lot_size
-        pnl = pnl_share * shares
-        
-        self.records['pnl_list'].append(pnl)
-        
-        if self.pos_states['pos_lots'] >= pos_close_lots:
-            self.pos_states['pos_lots'] = self.pos_states['pos_lots'] - pos_close_lots
-        else:
-            raise Exception("Number of Lots exceed position lots")
-        
-        if self.debug:
-            print(f"""\n
-                Position Close @ Price = {self.pos_states['pos_close_price']}
-            """)
+
+        if self.pos_list[0][0] >= pos_close_lots:
             
-        self.reset_pos_state()
-    
-    def reset_pos_state(self):
-        self.pos_states = {
-            'pos_lots': 0,
-            'pos_open_price': 0,
-            'pos_close_price': 0
-        }
+            shares = pos_close_lots * self.lot_size
+            pnl_share = pos_close_price - self.pos_list[0][1]
+            pnl = pnl_share * shares
+            
+            self.records['pnl_list'].append(pnl)
+            
+            if self.debug:
+                print(f"""\n
+                    [Position Close] {pos_close_lots} Lots @ Price = {pos_close_price}
+                """)
+                
+            # updated total position lots after closing position, avg price unchange
+            self.pos_list[0][0] = self.pos_list[0][0] - pos_close_lots
+            
+            self.records['trade_close_list'].append((pos_close_lots, pos_close_price))
+        else:
+            raise Exception("Error when closing position. Number of Lots exceed position lots")
+        
     
         
     def create_backtest(self, params):
@@ -94,27 +106,27 @@ class BacktestRunner():
                     now_volume: {self.market_data(i,'volume')} 
                 """)
                 
-                
-
-            
             # 2. Equity Recording
-            unrealize_pnl = round(self.pos_states['pos_lots'] * self.lot_size * (self.market_data(i,'close') - self.pos_states['pos_open_price']), 0)
+            unrealize_pnl = round(self.pos_list[0][0] * self.lot_size * (self.market_data(i,'close') - self.pos_list[0][1]), 0)
             realize_pnl = round(sum(self.records['pnl_list']), 0)
             
             equity = round(realize_pnl + unrealize_pnl, 0)
             self.records['equity_list'].append(equity)
             
             highest_equity = max(self.records['equity_list'])
+            print(type(highest_equity))
             dd_dollar = highest_equity - equity
             dd_pct = (equity / highest_equity) - 1
             self.records['dd_dollar_list'].append(dd_dollar)
+            if np.isnan(dd_pct):
+                dd_pct = 0
             self.records['dd_pct_list'].append(dd_pct)
             
             if self.debug:
                 print(f"""\n
-                    pos_open_price: {self.pos_states['pos_open_price']}
-                    pos_close_price: {self.pos_states['pos_close_price']}
-                    pos_lots : {self.pos_states['pos_lots']}
+                
+                    avg_open_price: {self.pos_list[0][1]}
+                    pos_lots : {self.pos_list[0][0]}
                     unrealize_pnl: {unrealize_pnl}
                     realize_pnl: {realize_pnl}
                     pnl_list: {self.records['pnl_list']}
@@ -123,39 +135,7 @@ class BacktestRunner():
                     dd_pct_list: {self.records['dd_pct_list']}
                 """)
                 
-            # 3. Trading Logic
-            entry_logic = self.entry_logic(i, params)
-            exit_logic = self.exit_logic(i, params)
-            is_last = i == len(df)-1
-            
-                
-            # 4. Close Position
-            if self.pos_states['pos_lots'] != 0 and is_last and exit_logic_tp and exit_logic_sl:
-                
-                self.pos_states['pos_close_price'] = self.market_data(i,'close')
-                
-                pnl = (self.pos_states['pos_close_price'] - self.pos_states['pos_open_price']) * self.pos_states['pos_lots'] * self.lot_size
-                self.records['pnl_list'].append(pnl)
-                
-                if self.debug:
-                    print(f"""\n
-                        Position Close @ Price = {self.pos_states['pos_close_price']}
-                    """)
-                    
-                self.pos_states['pos_lots'] = 0
-                self.pos_states['pos_open_price'] = 0
-                self.pos_states['pos_close_price'] = 0
-            
-            
-            # 5. Open Position
-            if self.pos_states['pos_lots'] == 0 and entry_logic:
-                self.pos_states['pos_lots'] = 1
-                self.pos_states['pos_open_price'] = self.market_data(i,'close')
-                
-                if self.debug:
-                    print(f"""\n
-                        Position Open @ Price = {self.pos_states['pos_open_price']}
-                    """)
+            self.onData(i, params)
         
         
 
@@ -164,15 +144,13 @@ class Strategy(BacktestRunner):
     def __init__(self, df, warm_up, lot_size, debug=True):
         super().__init__(df, warm_up, lot_size, debug)
     
-    def entry_logic(self,i ,params):
-        pass
-    
-        return True
-
-    def exit_logic(self,i ,params):
-        pass
-    
-        return True
+    def onData(self,i,params):
+        print(f"[On Data] i={i}, params={params}")
+        # 3. Define Trading Logic
+        
+        # 4. Close Position
+        
+        # 5. Open Position
         
 
     
@@ -194,5 +172,4 @@ params = {}
 df = pd.DataFrame(data)                
 strategy = Strategy(df, 1, 100)
 strategy.create_backtest(params)
-                
-                
+
